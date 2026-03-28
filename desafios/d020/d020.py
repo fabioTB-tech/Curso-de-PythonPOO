5c14303a3a81d8178b715c862817ed1ee2271b3a
fabioTB-tech <drawingfabio@gmail.com>
2026-03-28 11:14:40 -0300

    desafios 20 e 21

--- desafios/d020/d020.py
+++ desafios/d020/d020.py
@@ -1,5 +1,21 @@
+from rich import print
+from rich.panel import Panel
+
 class Gamer:
-    def __init__(self, nome, nick, *jogosFavoritos):
+    def __init__(self, nome, nick):
         self.nome = nome
         self.nick = nick
-        self.jogosFav = [jogosFavoritos]
+        self.jogosFav = []
+    
+    def add_favoritos(self, *jogo):
+        self.jogosFav.append(jogo)
+    
+    def ficha(self):
+
+        ficha = Panel(f'Nome real: {self.nome}\nJogos favoritos:\n{self.jogosFav}', title=f'Jogador <{self.nick}>', expand=False)
+        print(ficha)
+
+
+j1 = Gamer('Ahri', 'Ahriri_Miau')
+j1.add_favoritos('Celeste', 'Hollow Knigh', 'Forsaken')
+j1.ficha()
